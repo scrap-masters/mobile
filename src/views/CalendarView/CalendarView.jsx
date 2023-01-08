39d4ec03268d5cee683798d7a50b686771503337
Jakub Zakowicz <j.zakowicz99@gmail.com>
2023-01-08 20:57:50 +0100

    add react-native-calendar-events

--- src/views/CalendarView/CalendarView.jsx
+++ src/views/CalendarView/CalendarView.jsx
@@ -1,48 +1,78 @@
-import {Calendar} from "../../components/Calendar";
+import { Calendar } from '../../components/Calendar';
 import {
-    useGetSpecialization,
-    useGetSpecializationLegend,
-    useGetSpecializationTimetable
-} from "../../api/specializations";
-import {useEffect, useMemo, useState} from "react";
-import {Text, View} from "react-native";
-import SwitchSelector from "react-native-switch-selector";
-import {CalendarAgenda} from "../../components/CalendarAgenda";
+  useGetSpecialization,
+  useGetSpecializationLegend,
+  useGetSpecializationTimetable,
+} from '../../api/specializations';
+import RNCalendarEvents from 'react-native-calendar-events';
+import { useEffect, useMemo, useState } from 'react';
+import { Button, Text, View } from 'react-native';
+import SwitchSelector from 'react-native-switch-selector';
+import { CalendarAgenda } from '../../components/CalendarAgenda';
 
 export function CalendarView(props) {
-    const {route} = props
-    const {id} = route.params
-    const [group, setGroup] = useState(null)
-    const [selectedDay, setSelectedDay] = useState("")
+  const { route } = props;
+  const { id } = route.params;
+  const [group, setGroup] = useState(null);
+  const [selectedDay, setSelectedDay] = useState('');
 
-    const {data} = useGetSpecializationTimetable(id)
+  const { data } = useGetSpecializationTimetable(id);
 
-    // const {data: legendData} = useGetSpecializationLegend(id)
+  // const {data: legendData} = useGetSpecializationLegend(id)
 
-    // const {data: specializationData} = useGetSpecialization(id)
+  // const {data: specializationData} = useGetSpecialization(id)
 
-    const timetableData = data?.data.timetable
+  const timetableData = data?.data.timetable;
 
-    const groups = timetableData?.map((event) => event.group)
+  const groups = timetableData?.map(event => event.group);
 
-    const filteredGroups = groups?.filter((group, index) => groups.indexOf(group) === index)
+  const filteredGroups = groups?.filter(
+    (group, index) => groups.indexOf(group) === index
+  );
 
-    const filteredTimetable = timetableData?.filter((event) => event.group === group)
+  const filteredTimetable = timetableData?.filter(
+    event => event.group === group
+  );
 
-    const currentDayLessons = filteredTimetable?.filter(({start}) => start.split("T")[0] === selectedDay)
+  const currentDayLessons = filteredTimetable?.filter(
+    ({ start }) => start.split('T')[0] === selectedDay
+  );
 
-    const switchOptions = useMemo(() => filteredGroups?.map((group) => ({
+  const switchOptions = useMemo(
+    () =>
+      filteredGroups?.map(group => ({
         label: group,
-        value: group
-    })), [filteredGroups])
+        value: group,
+      })),
+    [filteredGroups]
+  );
 
-    useEffect(() => {
-        setGroup(data?.data.timetable[0].group)
-    }, [data])
+  useEffect(() => {
+    setGroup(data?.data.timetable[0].group);
+  }, [data]);
 
-    return <View>
-        {switchOptions && <SwitchSelector options={switchOptions} initial={0} onPress={setGroup}/>}
-        <Calendar selectedDay={selectedDay} setSelectedDay={setSelectedDay} timetable={filteredTimetable}/>
-        <CalendarAgenda lessons={currentDayLessons} />
+  useEffect(() => {
+    RNCalendarEvents.checkPermissions((readOnly = false))
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <View>
+      {switchOptions && (
+        <SwitchSelector
+          options={switchOptions}
+          initial={0}
+          onPress={setGroup}
+        />
+      )}
+      <Button title="Export Calendar" />
+      <Calendar
+        selectedDay={selectedDay}
+        setSelectedDay={setSelectedDay}
+        timetable={filteredTimetable}
+      />
+      <CalendarAgenda lessons={currentDayLessons} />
     </View>
+  );
 }
